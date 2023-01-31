8b81efad12a0d4364ecb281c746007ac79fdef95
Linuss <tade3d@gmail.com>
2023-01-31 11:45:29 +0100

    Added products routes.

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,21 +1,26 @@
-import express from "express"
-import resource from './_router'
+import express from "express";
+import products from "./products";
 
 // instantiate a new router
-const router = express.Router()
+const router = express.Router();
 
 /**
  * GET /
  */
-router.get('/', (req, res) => {
-	res.send({
-		message: "I AM API, BEEP BOOP",
-	})
-})
+router.get("/", (req, res) => {
+  res.send({
+    message: "I AM API, BEEP BOOP",
+  });
+});
 
 /**
  * [EXAMPLE] /resource
  */
 // router.use('/resource', resource)
 
-export default router
+/**
+ * /products
+ */
+router.use("/products", products);
+
+export default router;
