cd3609a4de17c63942161d629ad753cd748464e1
Linuss <tade3d@gmail.com>
2023-01-31 12:38:19 +0100

    Added multiple validations for when creating a new product.

--- src/routes/products.ts
+++ src/routes/products.ts
@@ -19,6 +19,39 @@ router.get("/:productId", show);
 /**
  * POST /products
  */
-router.post("/", [], store);
+router.post(
+  "/",
+  [
+    body("name")
+      .isString()
+      .withMessage("Has to be a string.")
+      .bail()
+      .isLength({ min: 3 })
+      .withMessage("Has to be atleast 3 characters long."),
+    body("description")
+      .isString()
+      .withMessage("Has to be a string.")
+      .bail()
+      .isLength({ min: 6 })
+      .withMessage("Has to be atleast 6 characters long."),
+    body("price")
+      .isInt({ min: 1 })
+      .withMessage("Has to be an int, value atleast 1."),
+    body("images")
+      .isObject()
+      .withMessage("Has to be an object with values 'thumbnail' & 'large'."),
+    body("stockStatus")
+      .isIn(["instock", "outofstock"])
+      .withMessage("Has to be either 'instock' or 'outofstock'."),
+    body("stockQuantity")
+      .isInt({ min: 0 })
+      .withMessage("Has to be an int, value atleast: 0."),
+    body("onSale")
+      .if(body("onSale").exists())
+      .isBoolean()
+      .withMessage("Has to be either 'true' or 'false'."),
+  ],
+  store
+);
 
 export default router;
